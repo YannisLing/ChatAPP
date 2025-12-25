c4cd221f685cf4dfc7a0e2548a1e48f8aa68de6a
YannisLing <3270614976@qq.com>
2025-12-25 16:23:44 +0800

    在用户路由中添加login

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,5 +3,5 @@ const{register} = require("../controllers/usersController");
 const router = require("express").Router();
 
 router.post("/register",register)
-
+router.post("/login",login)
 module.exports = router;
