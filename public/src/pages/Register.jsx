5653ab8b89f019ea0994eab9e9f2f15ea7d0cedf
YannisLing <3270614976@qq.com>
2025-12-25 15:30:00 +0800

    修改注册前端页面，添加后端验证成功失败代码

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import React ,{useState, useEffect} from "react";
-import { Link } from "react-router-dom";
+import { Link ,useNavigate} from "react-router-dom";
 import styled from "styled-components"
 import Logo from "../assets/logo.svg"
 import {ToastContainer,toast} from "react-toastify"
@@ -8,6 +8,7 @@ import axios from "axios"
 import { registerRoute } from "../utils/APIRoutes"
 
 function Register() {
+  const navigate = useNavigate();
   const [values,setValues]=useState({
     username:"",
     email:"",
@@ -25,13 +26,21 @@ function Register() {
     event.preventDefault();
     if(handleValidation()){
       console.log("in validation",registerRoute)
-      const {password,confirmPassword,username,email}=values;
+      const {password,username,email}=values;
       const {data} = await axios.post(registerRoute,{
         username,
         email,
         password,
       });
     };//提交后验证表单,验证通过调用api
+    if(data.status===false){
+      toast.error(data.msg,toastOptions);
+    }
+    if(data.status === true){
+      localStorage.setItem('chat-app-user',JSON.stringify(data.user));
+      navigate("/");
+    }
+
   };
   
   const handleValidation = () =>{
