a1cf0f7054599769c9dbd221c56ab9714d3f2453
YannisLing <3270614976@qq.com>
2025-12-25 16:10:17 +0800

    修改并完成登录页面

--- public/src/pages/Login.jsx
+++ public/src/pages/Login.jsx
@@ -5,15 +5,15 @@ import Logo from "../assets/logo.svg"
 import {ToastContainer,toast} from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import axios from "axios"
-import { registerRoute } from "../utils/APIRoutes"
+import { loginRoute } from "../utils/APIRoutes"
 
 function Login() {
   const navigate = useNavigate();
   const [values,setValues]=useState({
     username:"",
-    email:"",
+ 
     password:"",
-    confirmPassword:"",
+   
   })
   const toastOptions={
         position:"bottom-right",
@@ -25,11 +25,9 @@ function Login() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     if(handleValidation()){
-      
-      const {password,username,email}=values;
-      const {data} = await axios.post(registerRoute,{
+      const {password,username}=values;
+      const {data} = await axios.post(loginRoute,{
         username,
-        email,
         password,
       });
       if(data.status===false){
@@ -45,18 +43,12 @@ function Login() {
   };
   
   const handleValidation = () =>{
-    const {password, confirmPassword,username,email}=values;//解构values
-    if(password!==confirmPassword){//密码不一致抛出提示
-      toast.error("密码和确认密码应该一致",toastOptions);
+    const {password,username}=values;//解构values
+    if(password===""){
+      toast.error("密码为空！",toastOptions);
       return false;
-    }else if(username.length<2){
-      toast.error("用户名长度应该不小于2",toastOptions);
-      return false;
-    }else if(password.length<8){
-      toast.error("密码长度应该不小于8",toastOptions);
-      return false;
-    }else if(email===""){
-      toast.error("电子邮件地址是必须的",toastOptions);
+    }else if(username===""){
+      toast.error("用户名为空！",toastOptions);
       return false;
     }
     return true;
@@ -73,12 +65,10 @@ function Login() {
             <h1>YanniChat</h1>
           </div>
 
-          <input type="text" placeholder="用户名" name="username" onChange={e=>handleChange(e)} />
-          <input type="email" placeholder="邮件" name="email" onChange={e=>handleChange(e)} />
+          <input type="text" placeholder="用户名" name="username" onChange={e=>handleChange(e)} miun ="3"/>
           <input type="password" placeholder="密码" name="password" onChange={e=>handleChange(e)} />
-          <input type="password" placeholder="确认密码" name="confirmPassword" onChange={e=>handleChange(e)} />
-          <button type="submit">创建用户</button>
-          <span>已经有一个账号 ?点这里 <Link to="/login">登录</Link></span>
+          <button type="submit">登录</button>
+          <span>没有账号吗?点这里 <Link to="/register">注册</Link></span>
         </form>
       </FormContainer>
       <ToastContainer/>
